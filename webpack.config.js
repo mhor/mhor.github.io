5ac2e9fe54171bc054cba28b83aa651fae815fc8
mhor <maxime.horcholle@gmail.com>
2023-01-26 22:17:58 +0100

    upgrade webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,70 @@
+/**
+ * Webpack main configuration file
+ */
+
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const fs = require('fs');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const environment = require('./configuration/environment');
+
+const templateFiles = fs.readdirSync(environment.paths.source)
+  .filter((file) => ['.html', '.ejs'].includes(path.extname(file).toLowerCase())).map((filename) => ({
+    input: filename,
+    output: filename.replace(/\.ejs$/, '.html'),
+  }));
+
+const htmlPluginEntries = templateFiles.map((template) => new HTMLWebpackPlugin({
+  inject: true,
+  hash: false,
+  filename: template.output,
+  template: path.resolve(environment.paths.source, template.input),
+}));
 
 module.exports = {
-  entry: './index.js',
+  entry: {
+    app: path.resolve(environment.paths.source, 'js', 'app.js'),
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.js'
+    filename: 'js/[name].js',
+    path: environment.paths.output,
   },
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-              {
-                loader: 'css-loader',
-                options: { url: false }
-              },
-              {
-                loader: 'sass-loader'
-              }
-          ]
-        })
-      }
-    ]
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
   },
   plugins: [
-    new ExtractTextPlugin('css/styles.css'),
-  ]
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new CleanWebpackPlugin({
+      verbose: true,
+      cleanOnceBeforeBuildPatterns: ['**/*', '!stats.json'],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(environment.paths.source, 'images', 'content'),
+          to: path.resolve(environment.paths.output, 'images', 'content'),
+          toType: 'dir',
+          globOptions: {
+            ignore: ['*.DS_Store', 'Thumbs.db'],
+          },
+        },
+      ],
+    }),
+  ].concat(htmlPluginEntries),
+  target: 'web',
 };
